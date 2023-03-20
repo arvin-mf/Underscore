0890041801feb3e2b3f3a200908a8d0c9b60fc01
arvin-mf <arvinmulia55@gmail.com>
2023-03-20 12:51:30 +0700

    Revisi AddAfter AddBefore

--- 04_09.cpp
+++ 04_09.cpp
@@ -16,16 +16,23 @@ void AddFirst(char c);              // Penambahan
 void AddToIndex(int index, char c); // Penambahan
 void AddAfter(char checkpt, char insert);
 void AddBefore(char checkpt, char insert);
+void DeleteLast();
+void DeleteFirst();
 
 node FindByIndex(int index);        
 void PrintAll();
+
+void NotFound(char c){
+    cout << "Data '" << c << "' tidak ada" << endl;
+}
 
 int main(){
     head = tail = NULL;
     PrintAll();
     
     AddLast('T'), AddFirst('Q'), AddLast('A'), AddFirst('D'),
-    AddAfter('Q', 'U'), AddBefore('M', 'R'), AddToIndex(0, 'M'),
+    AddAfter('A', 'U'), AddBefore('U', 'R'), AddToIndex(6, 'M'),
+    DeleteLast(), 
     PrintAll();
 
     cout << FindByIndex(-1).data << endl;
@@ -71,23 +78,18 @@ void AddToIndex(int index, char c){
 
     struct node *temp1, *temp2;
     temp1 = head;
-    if(temp1 == NULL) head = tail = baru;
-    else if(index == 0) AddFirst(c);
+    if(index == 0) AddFirst(c);
     else{
         int i = 0;
         while(i != index){
+            i++;
+            if(temp1 == tail){
+                if(index == i) AddLast(c);
+                else cout << "Indeks AddToIndex melebihi supply" << endl;
+                return;
+            }
             temp2 = temp1;
             temp1 = temp1->next;
-            i++;
-            if(temp2 == tail) break;
-        }
-        if(index > i){
-            cout << "Indeks AddToIndex melebihi supply" << endl;
-            return;
-        }
-        if(temp2 == tail){
-            AddLast(c);
-            return;
         }
         temp2->next = temp1->prev = baru;
         baru->prev = temp2;
@@ -102,22 +104,21 @@ void AddAfter(char checkpt, char insert){
 
     node *temp1, *temp2;
     temp1 = head;
-    if(head == NULL) head = tail = baru;
-    else if(temp1 == tail) AddLast(insert);
+    if(temp1 == NULL) NotFound(checkpt);
     else{
         do{
-            temp2 = temp1;
-            temp1 = temp1->next;
             if(temp1 == tail){
                 if(temp1->data == checkpt){
                     AddLast(insert);
                     return;
                 }else if(temp2->data != checkpt){
-                    cout << "Data '" << checkpt << "' tidak ada" << endl;
+                    NotFound(checkpt);
                     return;
                 }
                 break;
             }
+            temp2 = temp1;
+            temp1 = temp1->next;
         }while(temp2->data != checkpt);
         temp2->next = baru;
         temp1->prev = baru;
@@ -133,7 +134,7 @@ void AddBefore(char checkpt, char insert){
 
     node *temp1, *temp2;
     temp1 = head;
-    if(head == NULL) head = tail = baru;
+    if(temp1 == NULL) cout << "Data '" << checkpt << "' tidak ada" << endl;
     else if(temp1->data == checkpt) AddFirst(insert);
     else{
         do{
@@ -152,6 +153,21 @@ void AddBefore(char checkpt, char insert){
         baru->next = temp1;
         baru->prev = temp2;
     }
+}
+
+void DeleteLast(){
+    node *temp;
+    temp = head;
+    if(temp == NULL) cout << "List kosong!!" << endl;
+    else{
+        while(temp != tail) temp = temp->next;
+        tail = temp->prev;
+        delete temp;
+    }
+}
+
+void DeleteFirst(){
+
 }
 
 node FindByIndex(int index){
