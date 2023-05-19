c32fd47160124bd14885ce1731c9bd0bb64af3a7
arvin-mf <arvinmulia55@gmail.com>
2023-05-19 21:37:35 +0700

    + fungsi UrutByNama

--- 09_10_2.cpp
+++ 09_10_2.cpp
@@ -15,6 +15,7 @@ void TukarPosisi(Pegawai *, Pegawai *);
 void CetakArray();
 void UrutByNIP();
 void UrutByNama();
+void Opsi();
 
 
 int main(){
@@ -35,13 +36,18 @@ int main(){
         cout << endl;
         cout << "1. Urutkan data pegawai berdasarkan NIP" << endl;
         cout << "2. Urutkan data pegawai berdasarkan Nama" << endl;
-        cout << "3. Tampilkan data pegawai\n4. Keluar" << endl;
-        cout << "Pilihan : ";
+        cout << "3. Tampilkan data pegawai\n4. Keluar\nPilihan : ";
         cin >> pilih;
 
         switch(pilih){
             case 1 :
+                UrutByNIP();
+                cout << "Data sudah diurutkan (v)" << endl;
+                break;
             case 2 :
+                UrutByNama();
+                cout << "Data sudah diurutkan (v)" << endl;
+                break;
             case 3 : CetakArray(); break;
             case 4 : cout << endl << "-- Menutup Program --" << endl; break;
             default : cout << "(!) Pilih bil. bulat 1 sampai 4 (!)" << endl;
@@ -52,12 +58,6 @@ int main(){
 }
 
 
-void TukarPosisi(Pegawai *a, Pegawai *b){
-    Pegawai bantu = *a;
-    *a = *b;
-    *b = bantu;
-}
-
 void CetakArray(){
     cout << endl << "-- DATA PEGAWAI --" << endl;
     for(int i = 0; i < MAX; i++){
@@ -67,11 +67,7 @@ void CetakArray(){
 }
 
 void UrutByNIP(){
-    do{
-        cout << "1. Ascending\n2. Descending" << endl;
-        cout << "Pilihan : ";
-        cin >> pilih;
-    }while(pilih != 1 && pilih != 2);
+    do Opsi(); while(pilih != 1 && pilih != 2);
 
     int i, j, k;
     for(i = 0; i < MAX - 1; i++){
@@ -85,15 +81,29 @@ void UrutByNIP(){
 }
 
 void UrutByNama(){
-    do{
-        cout << "1. Ascending\n2. Descending" << endl;
-        cout << "Pilihan : ";
-        cin >> pilih;
+    do Opsi(); while(pilih != 1 && pilih != 2);
 
-        switch(pilih){
-            case 1 :
-            case 2 :
-            default : cout << "(!) Pilih 1 atau 2 (!)" << endl;
+    int i, j, k;
+    for(i = 0; i < MAX - 1; i++){
+        k = i;
+        for(j = i + 1; j < MAX; j++){
+            int ii = 0;
+            while(Data[k].nama[ii] == Data[j].nama[ii]) ii++;
+            if(pilih == 1) if(Data[k].nama[ii] > Data[j].nama[ii]) k = j;
+            if(pilih == 2) if(Data[k].nama[ii] < Data[j].nama[ii]) k = j;
         }
-    }while(pilih != 1 && pilih != 2);
+        TukarPosisi(&Data[i], &Data[k]);
+    }
+}
+
+void Opsi(){
+    cout << "1. Ascending\n2. Descending" << endl;
+    cout << "Pilihan : ";
+    cin >> pilih;
+}
+
+void TukarPosisi(Pegawai *a, Pegawai *b){
+    Pegawai bantu = *a;
+    *a = *b;
+    *b = bantu;
 }
